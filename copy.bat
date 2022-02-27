0bacb2384a48b671e18413640ad194bd6494f16f
ComandPromt <vayase_a_ferrol@hotmail.com>
2022-02-27 11:34:41 +0100

    Add files via upload

---
+++ copy.bat
@@ -0,0 +1,9 @@
+@echo off
+
+mkdir "C:\Users\%username%\AppData\Roaming\Microsoft\Media Player\Skins\iPodCollectionUpdate11-20-051-4"
+
+copy *.wmz "C:\Users\%username%\AppData\Roaming\Microsoft\Media Player\Skins"
+
+
+copy iPodCollectionUpdate11-20-051-4\*.* "C:\Users\%username%\AppData\Roaming\Microsoft\Media Player\Skins\iPodCollectionUpdate11-20-051-4"   
+exit
